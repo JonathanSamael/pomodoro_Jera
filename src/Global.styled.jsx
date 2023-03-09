9cfbc2b838999d9621dac2f724d5c405d2662107
Jonathan Amaral <jonathansamael@live.com>
2023-03-09 11:32:11 -0300

    "🎨style: create global style"

---
+++ src/Global.styled.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+  * {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 1.5em;
+
+    max-width: 1440px;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 0;
+
+    background-color: #111827;
+    color: #f9fafb;
+  }
+
+`
