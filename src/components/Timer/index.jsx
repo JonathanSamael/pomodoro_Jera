54d267f39e596c6ca32fcea690fefddfdc6a56ee
Jonathan Amaral <jonathansamael@live.com>
2023-03-10 19:41:32 -0300

    "✨feat: notification sound when counter resets"

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -4,16 +4,17 @@ import { PlayButton } from '../Buttons/PlayButton';
 import { MainContainer, TimerContainer, TimerStyled, ButtonContainer } from './Timer.styled';
 import { Header } from '../Header';
 import { OptionsContext } from '../Context/OptionsContext';
+import sound from '../../assets/notification-song.mp3'
 
 export const Timer = () => {
   const timesOption = useContext(OptionsContext);
 
-  const [isPaused, setIsPaused] = useState(true);
+  const [isActive, setIsActive] = useState(true);
   const [mode, setMode] = useState('work');
   const [timeLeft, setTimeLeft] = useState(0);
 
   const timeLeftRef = useRef(timeLeft);
-  const isPausedRef = useRef(isPaused);
+  const isActiveRef = useRef(isActive);
   const modeRef = useRef(mode);
 
   useEffect(() => {
@@ -33,12 +34,13 @@ export const Timer = () => {
     setTimeLeft(timeLeftRef.current);
 
     let interval = setInterval(() => {
-      if (isPausedRef.current) {
-        return;
-      }
+      if (isActiveRef.current) return;
+
       if (timeLeftRef.current === 0) {
-        return switchMode()
-      }
+        const audio = new Audio(sound);
+        audio.play();
+        switchMode()
+      };
 
       setTimeLeft(timeLeftRef.current--)
 
@@ -46,16 +48,15 @@ export const Timer = () => {
 
     return () => clearInterval(interval);
   }, [timesOption])
-
   const totalTime = mode === 'work'
-    ? timesOption.workMinutes * 60
-    : timesOption.breakMinutes * 60;
-
+  ? timesOption.workMinutes * 60
+  : timesOption.breakMinutes * 60;
+  
   const clock = Math.round(timeLeft / totalTime * 100);
-
+  
   let minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft % 60;
-
+  
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
@@ -67,9 +68,9 @@ export const Timer = () => {
       </TimerContainer>
 
       <ButtonContainer>
-        {isPaused
-          ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false }} />
-          : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true }} />}
+        {isActive
+          ? <PlayButton onClick={() => { setIsActive(false); isActiveRef.current = false }} />
+          : <PauseButton onClick={() => { setIsActive(true); isActiveRef.current = true }} />}
       </ButtonContainer>
     </MainContainer>
   )
